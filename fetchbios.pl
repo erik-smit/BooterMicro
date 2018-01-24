4afb6739395a06445478c790029de76a7cfd5f88
Jon Nistor <nistorj@gmail.com>
2018-01-23 22:23:08 -0500

    Small modernization of url and table

--- fetchbios.pl
+++ fetchbios.pl
@@ -7,6 +7,9 @@ use HTML::Strip;
 use Date::Format;
 use Data::Dumper;
 
+my $o_debug = 1;
+
+
 sub trim($)
 {
   my $string = shift;
@@ -26,15 +29,21 @@ chdir "Firmware" or die "Cannot chdir(\"Firmware\"): $!";
 
 my $hs = HTML::Strip->new();
 
-my $url = "http://www.supermicro.nl/support/bios/";
-my @tableheaders = qw ( Model Name Type Rev "ZIP File" Description );
+#my $url = "http://www.supermicro.nl/support/bios/";
+my $url = "https://www.supermicro.com/support/resources/bios_ipmi.php?vendor=1";
+# my @tableheaders = qw ( Model Name Type Rev "ZIP File" Description );
+my @tableheaders = qw ( Model Rev "Download Zip" "Release Notes" "Part#" Description Name);
 
 my $agent = WWW::Mechanize->new();
 $agent->get($url);
 
+
 print join(',', @tableheaders), "\n";
 
-$te = new HTML::TableExtract( keep_html => 1, attribs => { id => 'ctl00_ctl00_ContentPlaceHolderMain_ContentPlaceHolderSupportMiddle_GridView1' } );
+# $te = new HTML::TableExtract( keep_html => 1, attribs => { id => 'ctl00_ctl00_ContentPlaceHolderMain_ContentPlaceHolderSupportMiddle_GridView1' } );
+$te = new HTML::TableExtract( keep_html => 1, attribs => { class => 'biosipmiTable' } );
+
+
 $te->parse( $agent->content() );
 
 foreach $ts ($te->table_states) {
@@ -50,21 +59,30 @@ foreach $ts ($te->table_states) {
          )
        );
 
+	
      $Model = join('_', @Models);
-     $BIOS  = trim($hs->parse( @$row[1]));
-     $Type  = trim($hs->parse( @$row[2]));
-     $Rev   = trim($hs->parse( @$row[3]));
+     $BIOS  = trim($hs->parse( @$row[6])); # Name field
+     $Type  = trim($hs->parse( @$row[5]));
+     $Rev   = trim($hs->parse( @$row[1]));
      $Rev   =~ s/\ /_/;
-     ($ZipLink, $ZipText) = @$row[4] =~ /(ID=\d+)\".*\"\>(.*)\<\/font\>/;
-     next unless "$ZipLink" =~ /ID=/;
-     $BIOSPath = "$Model/$Type/$Rev/$ZipText";
+     ($ZipLink, $ZipText) = @$row[2] =~ /(SoftwareItemID=\d+)\".*\"\>(.*)\<\/a\>/;
+
+     if( $Type eq "BIOS"  && $o_debug == 1 )
+     {
+    	printf "DEBUG: Model %10s / TYPE %10s / REV %10s / ZIP-ID %15s / File %15s\n", $Model, $Type, $Rev, $ZipLink, $ZipText;
+     }
+
+     next unless "$ZipLink" =~ /SoftwareItemID=/;
+     $BIOSPath = "$BIOS/$Type/$Rev/$ZipText";
+
+
      if (! -e $BIOSPath) {
-       mkdir "$Model";
-       mkdir "$Model/$Type";
-       mkdir "$Model/$Type/$Rev";
-       open CL, '>>', "$Model/ChangeLog";
+       mkdir "$BIOS"; # was $Model
+       mkdir "$BIOS/$Type";
+       mkdir "$BIOS/$Type/$Rev";
+       open CL, '>>', "$BIOS/ChangeLog";
        open CL2, '>>', "ChangeLog";
-       $Logline = time2str("%Y-%m-%d %H:%M:%S", time). " \"$Model/$Type/$Rev/$ZipText\"\n";
+       $Logline = time2str("%Y-%m-%d %H:%M:%S", time) . ": $BIOS/$Type/$Rev/$ZipText\n";
        print CL $Logline;
        print CL2 $Logline;
        print $Logline;
@@ -73,6 +91,8 @@ foreach $ts ($te->table_states) {
        if ($Type eq "BIOS") { 
          system("../mkbiosimg.sh", "Firmware/$BIOSPath");
        }
+     } else {
+	print "[!] DIR already exists: $BIOSPath\n";
      }
    }
 }
